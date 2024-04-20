568ea6bf9414206331de0e78a6d87fafa98cfe17
Chris Salvador <csalvador58@gmail.com>
2024-04-19 22:05:30 -0700

    refactor code due to code structure issues with starter

--- nillion-python-starter/programs/healthcare_imaging_compute.py
+++ nillion-python-starter/programs/healthcare_imaging_compute.py
@@ -2,43 +2,47 @@ from nada_dsl import *
 
 
 def nada_main():
-    num_coefficients = 31
+    num_params = 31
 
     # Define the parties
     patient = Party(name="Patient_test_img")
     health_provider_1 = Party(name="HP_1")
     health_provider_2 = Party(name="HP_2")
+    
+    # Define the public inputs
+    prediction_init = PublicInteger(Input(name="prediction_init", party=patient))
 
     # Define the secret inputs
     hp_1_data_size = SecretInteger(Input(name="hp_1_data_size", party=health_provider_1))
     hp_2_data_size = SecretInteger(Input(name="hp_2_data_size", party=health_provider_2))
     patient_image_data = [] # Compute time secrets
-    hp_1_thetas = []  # Stored secrets
-    hp_2_thetas = []  # Stored secrets  
+    hp_1_theta = []  # Stored secrets
+    hp_2_theta = []  # Stored secrets  
 
-    for i in range(1, num_coefficients):
+    for i in range(1, num_params):
         patient_image_data.append(
             SecretInteger(Input(name="patient_image_data_"+str(i), party=patient))
         )
-        hp_1_thetas.append(
-            SecretInteger(Input(name="hp_1_coeff_"+str(i), party=health_provider_1))
+        hp_1_theta.append(
+            SecretInteger(Input(name="hp_1_param_"+str(i), party=health_provider_1))
         )
-        hp_1_thetas.append(
-            SecretInteger(Input(name="hp_2_coeff_"+str(i), party=health_provider_2))
+        hp_1_theta.append(
+            SecretInteger(Input(name="hp_2_param_"+str(i), party=health_provider_2))
         )
 
-    # Initialize the prediction
-    prediction = 0
-    
+        
     # Compute weights for each party's data size
     hp_1_weight = hp_1_data_size / (hp_1_data_size + hp_2_data_size)
     hp_2_weight = hp_2_data_size / (hp_1_data_size + hp_2_data_size)
     
-    # Use weighted average to compute thetas
-    combined_theta = (hp_1_thetas * hp_1_weight) + (hp_2_thetas * hp_2_weight)
+    # Use weighted average to compute the combined theta
+    combined_theta = []
+    for hp_1_param, hp_2_param in zip(hp_1_theta, hp_2_theta):
+        combined_theta.append((hp_1_param * hp_1_weight) + (hp_2_param * hp_2_weight)) 
     
     # Perform element-wise multiplication and summation for scaled data
+    prediction = prediction_init
     for x_val, theta_val in zip(patient_image_data, combined_theta):
         prediction += x_val * theta_val
     
-    return [Output(prediction, "patient_prediction", patient)]
+    return [Output(prediction, "patient_test_prediction", patient)]
